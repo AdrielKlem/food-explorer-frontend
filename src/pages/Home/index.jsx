5b0b99be01a0ac61f1bac65752d36763e8703328
Adriel Klem <adrielklem16@gmail.com>
2023-10-04 13:10:29 -0300

    update sections card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -76,9 +76,7 @@ export function Home() {
                         >
                             {
                                 dishes.filter(dish => dish.category === "Refeição")
-                                .map((dish, index) => console.log(dish))
-                                /*
-                                (
+                                .map((dish, index) => (
                                     <SwiperSlide
                                         key={String(index)}
                                     >
@@ -87,98 +85,99 @@ export function Home() {
                                         />
                                     </SwiperSlide>
                                 ))
-                                */
                             }
                         </Swiper>
                 }
-
-                <h2>Refeições</h2>
-
-                <Swiper
-                    slidesPerView={4}
-                    centeredSlides={true}
-                    grabCursor={true}
-                    loop={true}
-                    loopfillgroupwithblank={true}
-                    breakpoints={{
-                        "@0.00": {
-                            slidesPerView: 1,
-                            spaceBetween: 10,
-                        },
-                        "@0.75": {
-                            slidesPerView: 2,
-                            spaceBetween: 20,
-                        },
-                        "@1.00": {
-                            slidesPerView: 3,
-                            spaceBetween: 40,
-                        },
-                        "@1.20": {
-                            slidesPerView: 4,
-                            spaceBetween: 160,
-                        },
-                    }}
-                    navigation={true}
-                    modules={[Navigation]}
-                    className="mySwiper section"
-                >
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida"
-                            price="4,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card 
-                            className="swiper-slide" 
-                            title="Comida 1"
-                            price="4,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida 2"
-                            price="5,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida 3"
-                            price="8,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida 3"
-                            price="8,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida 3"
-                            price="8,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida 3"
-                            price="8,00"
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <Card
-                            className="swiper-slide" 
-                            title="Comida 4"
-                            price="10,00"
-                        />
-                    </SwiperSlide>
-                </Swiper>   
+                <h2>Sobremesas</h2>
+                {
+                    dishes.filter(dish => dish.category === "Sobremesa").length > 0 &&
+                        <Swiper
+                            slidesPerView={3}
+                            centeredSlides={false}
+                            grabCursor={true}
+                            loop={true}
+                            loopfillgroupwithblank={true}
+                            breakpoints={{
+                                "@0.00": {
+                                    slidesPerView: 1,
+                                    spaceBetween: 10,
+                                },
+                                "@0.75": {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                },
+                                "@1.00": {
+                                    slidesPerView: 3,
+                                    spaceBetween: 40,
+                                },
+                                "@1.20": {
+                                    slidesPerView: 4,
+                                    spaceBetween: 160,
+                                },
+                            }}
+                            navigation={true}
+                            modules={[Navigation]}
+                            className="mySwiper section"
+                        >
+                            {
+                                dishes.filter(dish => dish.category === "Refeição")
+                                .map((dish, index) => (
+                                    <SwiperSlide
+                                        key={String(index)}
+                                    >
+                                        <Card
+                                            data={dish}
+                                        />
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper>
+                }
+                <h2>Lanches</h2>
+                {
+                    dishes.filter(dish => dish.category === "Lanche").length > 0 &&
+                        <Swiper
+                            slidesPerView={3}
+                            centeredSlides={false}
+                            grabCursor={true}
+                            loop={true}
+                            loopfillgroupwithblank={true}
+                            breakpoints={{
+                                "@0.00": {
+                                    slidesPerView: 1,
+                                    spaceBetween: 10,
+                                },
+                                "@0.75": {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                },
+                                "@1.00": {
+                                    slidesPerView: 3,
+                                    spaceBetween: 40,
+                                },
+                                "@1.20": {
+                                    slidesPerView: 4,
+                                    spaceBetween: 160,
+                                },
+                            }}
+                            navigation={true}
+                            modules={[Navigation]}
+                            className="mySwiper section"
+                        >
+                            {
+                                dishes.filter(dish => dish.category === "Refeição")
+                                .map((dish, index) => (
+                                    <SwiperSlide
+                                        key={String(index)}
+                                    >
+                                        <Card
+                                            data={dish}
+                                        />
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper>
+                }
             </Section>
         </main>
         <Footer />
